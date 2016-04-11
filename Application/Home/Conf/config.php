2ac035613856f661ef753ff859e704f30ac92ac8
dongshujin <dongshujin@xiaoqianghome.com>
2016-04-11 19:04:35 +0800

    Change: init project

---
+++ Application/Home/Conf/config.php
@@ -0,0 +1,9 @@
+<?php
+return array(
+    'PAGE_SIZE_20' => 20,   //每页显示多少条数据在这里修改
+    'VOTE_LIMIT_COUNT' => 10,//投票成功文案，可以修改
+    'VOTE_SUCCESS_MSG' => '投票成功，猴赛雷',//投票成功文案，可以修改
+    'VOTE_FAIL_MSG' => '这个猴不能再投票了',//投票失败文案[这个城市已经投了10票] 可以修改
+    'VOTE_ERROR_MSG' => 'ERROR',//投票失败文案[这个城市已经投了10票] 可以修改
+    'AVATAR_URL_PREFIX' => 'http://localhost:9003/public/images/employee/',
+);
---
+++ Application/Home/Controller/IndexController.class.php
@@ -0,0 +1,106 @@
+<?php
+// 本类由系统自动生成，仅供测试用途
+namespace Home\Controller;
+use Think\Controller;
+use Think\Model;
+use Think\Page;
+
+class IndexController extends Controller {
+
+    public function index(){
+
+        $departmentId = I('get.departmentId');
+        $pn = I('get.p', 1, 'int');
+        $ps = C('PAGE_SIZE_20');
+
+        $model = new Model();
+        $select_allDepartments = "SELECT * FROM department";
+        $allDepartments = $model->query($select_allDepartments);
+
+        $departmentId = empty($departmentId) ? $allDepartments[0]['id'] : $departmentId;
+
+        $start = ($pn - 1)*$ps;
+
+        $select_deptEmployees = "SELECT * FROM employee WHERE department_id = $departmentId AND status = 1 ORDER BY votes DESC LIMIT $start, $ps";
+        $allDeptEmployees = $model->query($select_deptEmployees);
+
+        $total = $model->query("SELECT COUNT(id) AS count FROM employee WHERE department_id = $departmentId")[0]['count'];
+
+        $page  = new Page($total, $ps);
+        $pageShow = $page->show();
+
+        $this->assign('departmentId', $departmentId);
+        $this->assign('pageShow', $pageShow);
+        $this->assign('departmentList', $allDepartments);
+        $this->assign('employeeList', $allDeptEmployees);
+        $this->assign('avatarUrlPrefix', C('AVATAR_URL_PREFIX'));
+        $this->display();
+    }
+
+
+    /**
+     * 投票
+     * 每人每个城市一天10票
+     */
+    public function vote(){
+        $limitCount = C('VOTE_LIMIT_COUNT');
+
+        $ip = $_SERVER["REMOTE_ADDR"];
+        $departmentId = I('post.departmentId');
+        $employeeId = I('post.employeeId');
+
+//        cookie('departmentId', null);
+
+        $deptIds = cookie('departmentId');
+        $deptIds = empty($deptIds) ? array() : json_decode($deptIds,true);
+        if(!empty($deptIds[$departmentId][$ip]) && $deptIds[$departmentId][$ip] >= $limitCount){
+            $result = array(
+                'flag' => 'fail',
+                'msg' => C('VOTE_FAIL_MSG'),
+            );
+            $this->ajaxReturn($result, 'json');exit;
+        }
+
+        if(!empty($deptIds[$departmentId][$ip])) {
+            $deptIds[$departmentId][$ip] += 1;
+        } else {
+            $deptIds[$departmentId][$ip] = 1;
+        }
+
+
+        $y = date("y");
+        $m = date("m");
+        $d = date("d");
+        $dayEnd= mktime(23,59,59,$m,$d,$y);
+        cookie('departmentId', json_encode($deptIds), ($dayEnd-time()));
+
+        $voteRes = M('employee')->where(array('id'=>$employeeId))->setInc('votes',1);
+        if($voteRes) {
+            $result = array(
+                'flag' => 'success',
+                'msg' => C('VOTE_SUCCESS_MSG'),
+            );
+            $this->ajaxReturn($result, 'json');
+        } else {
+            $result = array(
+                'flag' => 'fail',
+                'msg' => C('VOTE_ERROR_MSG'),
+            );
+            $this->ajaxReturn($result, 'json');exit;
+        }
+    }
+
+    /**
+     * 获取网址二维码
+     */
+    /*public function getQrCode(){
+        vendor('phpqrcode.phpqrcode');
+        $qrTools = new \QRcode();
+        $errorCorrectionLevel = "L";
+        $matrixPointSize = "8";
+        $margin="1";
+        $value = "http://".$_SERVER['HTTP_HOST'].$_SERVER['PHP_SELF']."?".$_SERVER['QUERY_STRING'];
+        $qrCode = $qrTools::png($value, false, $errorCorrectionLevel, $matrixPointSize,$margin);
+        $this->ajaxReturn(array('flag'=>'success','qrCode'=>$qrCode),'text');
+    }*/
+}
